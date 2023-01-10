a4033781524d394ba04134d45812625b505069db
go-to-k <24818752+go-to-k@users.noreply.github.com>
2023-01-11 00:04:09 +0900

    refactor: remove default flag of region (#15)

---
+++ aws_config.go
@@ -0,0 +1,36 @@
+package cls3
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+const defaultAwsRegion = "us-east-1"
+
+func LoadAWSConfig(ctx context.Context, region string, profile string) (aws.Config, error) {
+	var (
+		cfg aws.Config
+		err error
+	)
+
+	if profile != "" {
+		cfg, err = config.LoadDefaultConfig(ctx, config.WithSharedConfigProfile(profile))
+	} else {
+		cfg, err = config.LoadDefaultConfig(ctx)
+	}
+
+	if err != nil {
+		return cfg, err
+	}
+
+	if region != "" {
+		cfg.Region = region
+	}
+	if cfg.Region == "" {
+		cfg.Region = defaultAwsRegion
+	}
+
+	return cfg, nil
+}
