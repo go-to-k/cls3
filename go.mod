d39de4069b36af03650c645a20c8cecb7f001a05
go-to-k <24818752+go-to-k@users.noreply.github.com>
2024-08-18 21:50:10 +0900

    wip

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.27.27
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.58.2
 	github.com/aws/smithy-go v1.20.3
+	github.com/charmbracelet/bubbletea v0.27.0
+	github.com/fatih/color v1.17.0
 	github.com/gosuri/uilive v0.0.4
 	github.com/rs/zerolog v1.30.0
 	github.com/urfave/cli/v2 v2.27.4
@@ -33,14 +35,25 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.22.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.30.3 // indirect
+	github.com/charmbracelet/x/ansi v0.1.4 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/term v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
---
+++ internal/io/ui.go
@@ -0,0 +1,132 @@
+package io
+
+import (
+	"fmt"
+	"strings"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/fatih/color"
+)
+
+type UI struct {
+	Choices   []string
+	Header    string
+	Cursor    int
+	Selected  map[int]struct{}
+	Keyword   string
+	isEntered bool
+}
+
+var _ tea.Model = (*UI)(nil)
+
+func NewUI(choices []string, header string) *UI {
+	return &UI{
+		Choices:  choices,
+		Header:   header,
+		Selected: make(map[int]struct{}),
+	}
+}
+
+func (u *UI) Init() tea.Cmd {
+	return nil
+}
+
+func (u *UI) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	//nolint:gocritic
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		switch msg.String() {
+
+		// Quit the selection
+		case "enter":
+			u.isEntered = true
+			return u, tea.Quit
+
+		// Quit the selection and clear ALL selected items
+		case "ctrl+c":
+			u.Selected = make(map[int]struct{})
+			return u, tea.Quit
+
+		case "up":
+			if u.Cursor >= 0 {
+				u.Cursor--
+			}
+			if u.Cursor == -1 {
+				u.Cursor = len(u.Choices) - 1
+			}
+
+		case "down":
+			if u.Cursor <= len(u.Choices)-1 {
+				u.Cursor++
+			}
+			if u.Cursor == len(u.Choices) {
+				u.Cursor = 0
+			}
+
+		// select or deselect an item
+		case " ":
+			_, ok := u.Selected[u.Cursor]
+			if ok {
+				delete(u.Selected, u.Cursor)
+			} else {
+				u.Selected[u.Cursor] = struct{}{}
+			}
+
+		// select all items
+		case "right":
+			for i := range u.Choices {
+				u.Selected[i] = struct{}{}
+			}
+
+		// clear all selected items
+		case "left":
+			u.Selected = make(map[int]struct{})
+
+		case "backspace":
+			if len(u.Keyword) > 0 {
+				u.Keyword = u.Keyword[:len(u.Keyword)-1]
+			}
+
+		default:
+			u.Keyword += msg.String()
+
+		}
+	}
+
+	return u, nil
+}
+
+func (u *UI) View() string {
+	s := color.CyanString("? ") + color.New(color.Bold).Sprint(u.Header)
+
+	if u.isEntered {
+		return s
+	}
+
+	s += u.Keyword
+
+	s += color.CyanString(" [Use arrows to move, space to select, <right> to all, <left> to none, type to filter]")
+	s += "\n"
+
+	for i, choice := range u.Choices {
+		if u.Keyword != "" && !strings.Contains(choice, u.Keyword) {
+			continue
+		}
+
+		cursor := " " // no cursor
+		if u.Cursor == i {
+			cursor = color.CyanString(">") // cursor!
+		}
+
+		checked := "[ ]" // not selected
+		if _, ok := u.Selected[i]; ok {
+			checked = color.GreenString("[x]") // selected!
+		}
+
+		s += fmt.Sprintf("%s %s %s\n", cursor, checked, choice)
+	}
+
+	return s
+}
