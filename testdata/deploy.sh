883c23937e7c140127b9b3e5fbe0b7df6c0bc43d
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-01-26 23:10:24 +0900

    Revert "fix deploy.sh"
    This reverts commit 740665ffc6d1574dea7ac97d17f6916d95069ae6.

--- testdata/deploy.sh
+++ testdata/deploy.sh
@@ -4,101 +4,59 @@ set -eu
 cd $(dirname $0)
 
 profile=""
-bucket_prefix="cls3-test"
+bucket_name=""
 profile_option=""
-num_buckets=10
-objects_per_bucket=10000
-# objects_per_bucket=1000000
 
-while getopts p:b:n:o: OPT; do
+while getopts p:b: OPT; do
 	case $OPT in
 	p)
 		profile="$OPTARG"
 		;;
 	b)
-		bucket_prefix="$OPTARG"
-		;;
-	n)
-		num_buckets="$OPTARG"
-		;;
-	o)
-		objects_per_bucket="$OPTARG"
+		bucket_name="$OPTARG"
 		;;
 	*) ;;
 	esac
 done
 
-if [ -z "${bucket_prefix}" ]; then
-	echo "bucket_prefix option (-b) is required"
+if [ -z "${bucket_name}" ]; then
+	echo "bucket_name option (-b) is required"
 	exit 1
 fi
 
-if ! [[ "${num_buckets}" =~ ^[0-9]+$ ]]; then
-	echo "number of buckets (-n) must be a positive integer"
-	exit 1
-fi
-
-if ! [[ "${objects_per_bucket}" =~ ^[0-9]+$ ]]; then
-	echo "number of objects (-o) must be a positive integer"
-	exit 1
-fi
+lower_bucket_name=$(echo "${bucket_name}" | tr '[:upper:]' '[:lower:]')
 
 if [ -n "${profile}" ]; then
 	profile_option="--profile ${profile}"
 fi
 
-# Calculate iterations considering 4 versions (3 copies and 1 deletion) and 1000 files
-iterations=$((objects_per_bucket / (4 * 1000)))
-if [ ${iterations} -lt 1 ]; then
-	iterations=1
+exist=$(aws s3 ls ${profile_option} | grep -E " ${lower_bucket_name}$" || true)
+if [ -z "${exist}" ]; then
+	aws s3 mb s3://${lower_bucket_name} ${profile_option}
 fi
 
-random_suffix=$RANDOM
-padded_start=$(printf "%04d" 1)
-padded_end=$(printf "%04d" ${num_buckets})
-echo "=== buckets: ${bucket_prefix}-${random_suffix}-[${padded_start}-${padded_end}] ==="
-echo "=== versions: $(printf "%'d" ${objects_per_bucket}) per bucket ==="
+aws s3api put-bucket-versioning --bucket ${lower_bucket_name} --versioning-configuration Status=Enabled ${profile_option}
 
-pids=()
-for bucket_num in $(seq 1 ${num_buckets}); do
-	(
-		padded_num=$(printf "%04d" ${bucket_num})
-		bucket_name="${bucket_prefix}-${random_suffix}-${padded_num}"
-		lower_bucket_name=$(echo "${bucket_name}" | tr '[:upper:]' '[:lower:]')
+dir="./testfiles/${bucket_name}"
+mkdir -p ${dir}
 
-		exist=$(aws s3 ls ${profile_option} | grep -E " ${lower_bucket_name}$" || true)
-		if [ -z "${exist}" ]; then
-			aws s3 mb s3://${lower_bucket_name} ${profile_option} >/dev/null
-		fi
+# about 1000,000 versions
+## NOTE: It'll cost you about $3.75 (0.005 USD / 1000 PUT)(`aws s3 rm` is free because it's DELETE operation)
+for i in $(seq 1 250); do
+	touch ${dir}/${i}_{1..1000}_${RANDOM}.txt
 
-		aws s3api put-bucket-versioning --bucket ${lower_bucket_name} --versioning-configuration Status=Enabled ${profile_option}
+	# version
+	pids=()
+	for _ in $(seq 1 3); do
+		aws s3 cp ${dir} s3://${lower_bucket_name}/ --recursive ${profile_option} >/dev/null &
+		pids[$!]=$!
+	done
+	wait "${pids[@]}"
 
-		dir="./testfiles/${bucket_name}"
-		mkdir -p ${dir}
+	# delete marker
+	aws s3 rm s3://${lower_bucket_name}/ --recursive ${profile_option} >/dev/null
 
-		# about ${objects_per_bucket} versions
-		## NOTE: e.g.) For 1,000,000 versions per bucket, it'll cost you about $3.75 (0.005 USD / 1000 PUT)(DELETE operation is free)
-		## NOTE: 1,000,000 versions = 250,000 objects × 4 versions (3 PUT operations and 1 DELETE operation per object)
-		for i in $(seq 1 ${iterations}); do
-			touch ${dir}/${i}_{1..1000}_${RANDOM}.txt
-
-			# version
-			version_pids=()
-			for _ in $(seq 1 3); do
-				aws s3 cp ${dir} s3://${lower_bucket_name}/ --recursive ${profile_option} >/dev/null &
-				version_pids[$!]=$!
-			done
-			wait "${version_pids[@]}"
-
-			# delete marker
-			aws s3 rm s3://${lower_bucket_name}/ --recursive ${profile_option} >/dev/null
-
-			rm ${dir}/*.txt
-		done
-
-		rm -rf ${dir}
-	) &
-	pids[$!]=$!
+	rm ${dir}/*.txt
 done
 
-wait "${pids[@]}"
+rm -rf ${dir}
--- testdata/deploy_directory.sh
+++ testdata/deploy_directory.sh
@@ -4,106 +4,59 @@ set -eu
 cd $(dirname $0)
 
 profile=""
-bucket_prefix="cls3-test"
+bucket_name=""
 profile_option=""
-num_buckets=10
-objects_per_bucket=10000
-# objects_per_bucket=1000000
 
-while getopts p:b:n:o: OPT; do
+while getopts p:b: OPT; do
 	case $OPT in
 	p)
 		profile="$OPTARG"
 		;;
 	b)
-		bucket_prefix="$OPTARG"
-		;;
-	n)
-		num_buckets="$OPTARG"
-		;;
-	o)
-		objects_per_bucket="$OPTARG"
+		bucket_name="$OPTARG"
 		;;
 	*) ;;
 	esac
 done
 
-if [ -z "${bucket_prefix}" ]; then
-	echo "bucket_prefix option (-b) is required"
-	exit 1
-fi
-
-if ! [[ "${num_buckets}" =~ ^[0-9]+$ ]]; then
-	echo "number of buckets (-n) must be a positive integer"
-	exit 1
-fi
-
-if [ "${num_buckets}" -gt 10 ]; then
-	echo "number of buckets (-n) must be less than or equal to 10 for directory buckets"
-	exit 1
-fi
-
-if ! [[ "${objects_per_bucket}" =~ ^[0-9]+$ ]]; then
-	echo "number of objects (-o) must be a positive integer"
+if [ -z "${bucket_name}" ]; then
+	echo "bucket_name option (-b) is required"
 	exit 1
 fi
 
 region="us-east-1"
 az_id="use1-az4"
+# naming for S3 Express One Zone (using az 4 in es-east-1)
+lower_bucket_name=$(echo "${bucket_name}--${az_id}--x-s3" | tr '[:upper:]' '[:lower:]')
 
 if [ -n "${profile}" ]; then
 	profile_option="--profile ${profile} --region ${region}"
 fi
 
-# Calculate iterations considering 1000 files per iteration
-iterations=$((objects_per_bucket / 1000))
-if [ ${iterations} -lt 1 ]; then
-	iterations=1
+if [ -z "$(aws s3api list-directory-buckets ${profile_option} | grep ${lower_bucket_name})" ]; then
+	aws s3api create-bucket \
+		--bucket ${lower_bucket_name} \
+		--create-bucket-configuration "Location={Type=AvailabilityZone,Name=${az_id}},Bucket={DataRedundancy=SingleAvailabilityZone,Type=Directory}" \
+		${profile_option}
 fi
 
-random_suffix=$RANDOM
-padded_start=$(printf "%02d" 1)
-padded_end=$(printf "%02d" ${num_buckets})
-echo "=== buckets: ${bucket_prefix}-${random_suffix}-[${padded_start}-${padded_end}]--${az_id}--x-s3 ==="
-echo "=== objects: $(printf "%'d" ${objects_per_bucket}) per bucket ==="
+dir="./testfiles/${lower_bucket_name}"
+mkdir -p ${dir}
 
-pids=()
-for bucket_num in $(seq 1 ${num_buckets}); do
-	(
-		padded_num=$(printf "%02d" ${bucket_num})
-		bucket_name="${bucket_prefix}-${random_suffix}-${padded_num}"
-		# naming for S3 Express One Zone (using az 4 in es-east-1)
-		lower_bucket_name=$(echo "${bucket_name}--${az_id}--x-s3" | tr '[:upper:]' '[:lower:]')
+# about 1000,000 objects on the directory buckets for S3 Express One Zone
+## NOTE: It'll cost you $2.5 (S3 Express One Zone: 0.0025USD USD / 1000 PUT)
+## NOTE: You can create up to 10 directory buckets in each of your AWS accounts.
+## FIXME: Errors often occur when uploading files to S3 Express One Zone. (Retrying fixes it, but it happens again with another file.)
+### upload failed: testfiles/test-goto-01/1_864_21587.txt to s3://test-goto-01--use1-az4--x-s3/1_864_21587.txt An error occurred (400) when calling the PutObject operation: Bad Request
+for i in $(seq 1 1000); do
+	touch ${dir}/${i}_{1..1000}_${RANDOM}.txt
 
-		if [ -z "$(aws s3api list-directory-buckets ${profile_option} | grep ${lower_bucket_name})" ]; then
-			aws s3api create-bucket \
-				--bucket ${lower_bucket_name} \
-				--create-bucket-configuration "Location={Type=AvailabilityZone,Name=${az_id}},Bucket={DataRedundancy=SingleAvailabilityZone,Type=Directory}" \
-				${profile_option} >/dev/null
-		fi
+	## Do not finish even in the event of an error because the above error will occur.
+	set +e
+	aws s3 cp ${dir} s3://${lower_bucket_name}/ --recursive ${profile_option} >/dev/null
+	set -e
 
-		dir="./testfiles/${lower_bucket_name}"
-		mkdir -p ${dir}
-
-		# about ${objects_per_bucket} objects on the directory buckets for S3 Express One Zone
-		## NOTE: It'll cost you $2.5 (S3 Express One Zone: 0.0025USD USD / 1000 PUT)
-		## NOTE: You can create up to 10 directory buckets in each of your AWS accounts.
-		## FIXME: Errors often occur when uploading files to S3 Express One Zone. (Retrying fixes it, but it happens again with another file.)
-		### upload failed: testfiles/test-goto-01/1_864_21587.txt to s3://test-goto-01--use1-az4--x-s3/1_864_21587.txt An error occurred (400) when calling the PutObject operation: Bad Request
-		for i in $(seq 1 ${iterations}); do
-			touch ${dir}/${i}_{1..1000}_${RANDOM}.txt
-
-			## Do not finish even in the event of an error because the above error will occur.
-			set +e
-			aws s3 cp ${dir} s3://${lower_bucket_name}/ --recursive ${profile_option} >/dev/null
-			set -e
-
-			rm ${dir}/*.txt
-		done
-
-		rm -rf ${dir}
-	) &
-	pids[$!]=$!
+	rm ${dir}/*.txt
 done
 
-wait "${pids[@]}"
+rm -rf ${dir}
