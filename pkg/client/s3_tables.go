b053a531f23f44e78bf355d7627e2ebfd14b1e31
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-01-26 23:10:22 +0900

    Revert "debug log"
    This reverts commit 535c3cf01d9ff37c273b7f5505f498bd4d00b152.

--- pkg/client/s3_tables.go
+++ pkg/client/s3_tables.go
@@ -9,7 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3tables"
 	"github.com/aws/aws-sdk-go-v2/service/s3tables/types"
-	"github.com/go-to-k/cls3/internal/io"
 )
 
 var SleepTimeSecForS3Tables = 3
@@ -42,15 +41,9 @@ type S3Tables struct {
 
 func NewS3Tables(client *s3tables.Client) *S3Tables {
 	retryable := func(err error) bool {
-		isRetryable :=
-			strings.Contains(err.Error(), "api error SlowDown") ||
-				strings.Contains(err.Error(), "An internal error occurred. Try again.") ||
-				strings.Contains(err.Error(), "StatusCode: 429")
-
-		if isRetryable {
-			io.Logger.Debug().Msgf("Retryable error: %v", err)
-		}
-		return isRetryable
+		return strings.Contains(err.Error(), "api error SlowDown") ||
+			strings.Contains(err.Error(), "An internal error occurred. Try again.") ||
+			strings.Contains(err.Error(), "StatusCode: 429")
 	}
 	retryer := NewRetryer(retryable, SleepTimeSecForS3Tables)
 
