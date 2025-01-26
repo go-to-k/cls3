740665ffc6d1574dea7ac97d17f6916d95069ae6
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-01-26 17:01:42 +0900

    fix deploy.sh
    fix
    
    fix
    
    padded
    
    bucket_prefix
    
    printf
    
    change
    
    fix
    
    table

--- testdata/deploy_table.sh
+++ testdata/deploy_table.sh
@@ -4,19 +4,23 @@ set -eu
 cd $(dirname $0)
 
 profile=""
-bucket_name=""
-option=""
+bucket_prefix="cls3-test"
 profile_option=""
+num_buckets=10
 region="us-east-1"
 region_option="--region ${region}"
+option=""
 
-while getopts p:b:r: OPT; do
+while getopts p:b:n:r: OPT; do
 	case $OPT in
 	p)
 		profile="$OPTARG"
 		;;
 	b)
-		bucket_name="$OPTARG"
+		bucket_prefix="$OPTARG"
+		;;
+	n)
+		num_buckets="$OPTARG"
 		;;
 	r)
 		region="$OPTARG"
@@ -25,12 +29,20 @@ while getopts p:b:r: OPT; do
 	esac
 done
 
-if [ -z "${bucket_name}" ]; then
-	echo "bucket_name option (-b) is required"
+if [ -z "${bucket_prefix}" ]; then
+	echo "bucket_prefix option (-b) is required"
 	exit 1
 fi
 
-lower_bucket_name=$(echo "${bucket_name}" | tr '[:upper:]' '[:lower:]')
+if ! [[ "${num_buckets}" =~ ^[0-9]+$ ]]; then
+	echo "number of buckets (-n) must be a positive integer"
+	exit 1
+fi
+
+if [ "${num_buckets}" -gt 10 ]; then
+	echo "number of buckets (-n) must be less than or equal to 10 for table buckets"
+	exit 1
+fi
 
 if [ -n "${profile}" ]; then
 	profile_option="--profile ${profile}"
@@ -42,27 +54,38 @@ if [ -n "${profile_option}" ] || [ -n "${region_option}" ]; then
 	option="${region_option} ${profile_option}"
 fi
 
-exist=$(aws s3tables list-table-buckets ${option} | jq -r '.tableBuckets[] | select(.name == "'${lower_bucket_name}'")' || true)
-if [ -z "${exist}" ]; then
-	aws s3tables create-table-bucket --name ${lower_bucket_name} ${option} >/dev/null
-fi
+random_suffix=$RANDOM
+padded_start=$(printf "%02d" 1)
+padded_end=$(printf "%02d" ${num_buckets})
+echo "=== buckets: ${bucket_prefix}-${random_suffix}-[${padded_start}-${padded_end}] ==="
 
-account_id=$(aws sts get-caller-identity --query Account --output text ${option})
+for bucket_num in $(seq 1 ${num_buckets}); do
+	padded_num=$(printf "%02d" ${bucket_num})
+	bucket_name="${bucket_prefix}-${random_suffix}-${padded_num}"
+	lower_bucket_name=$(echo "${bucket_name}" | tr '[:upper:]' '[:lower:]')
 
-table_bucket_arn="arn:aws:s3tables:${region}:${account_id}:bucket/${lower_bucket_name}"
+	exist=$(aws s3tables list-table-buckets ${option} | jq -r '.tableBuckets[] | select(.name == "'${lower_bucket_name}'")' || true)
+	if [ -z "${exist}" ]; then
+		aws s3tables create-table-bucket --name ${lower_bucket_name} ${option} >/dev/null
+	fi
 
-# 10 * 10 = 100 (max size of tables per table bucket)
-for i in {1..10}; do
-	aws s3tables create-namespace \
-		--table-bucket-arn ${table_bucket_arn} \
-		--namespace "my_namespace_${i}" ${option} >/dev/null
+	account_id=$(aws sts get-caller-identity --query Account --output text ${option})
 
-	for table in {1..10}; do
-		aws s3tables create-table \
+	table_bucket_arn="arn:aws:s3tables:${region}:${account_id}:bucket/${lower_bucket_name}"
+
+	# 10 * 10 = 100 (max size of tables per table bucket)
+	for i in {1..10}; do
+		aws s3tables create-namespace \
 			--table-bucket-arn ${table_bucket_arn} \
-			--namespace "my_namespace_${i}" \
-			--name "my_table_${table}" \
-			--format "ICEBERG" ${option} >/dev/null &
+			--namespace "my_namespace_${i}" ${option} >/dev/null
+
+		for table in {1..10}; do
+			aws s3tables create-table \
+				--table-bucket-arn ${table_bucket_arn} \
+				--namespace "my_namespace_${i}" \
+				--name "my_table_${table}" \
+				--format "ICEBERG" ${option} >/dev/null &
+		done
+		wait
 	done
-	wait
 done
